06d53cd73d150197b723203462673d57ab03dac3
J0Z88 <72228397+J0Z88@users.noreply.github.com>
2021-11-20 21:23:09 -0600

    Primer commit
    Commit para iniciar proyecto

---
+++ src/main/java/com/jerv/gendra_pt/GendraPtApplication.java
@@ -0,0 +1,13 @@
+package com.jerv.gendra_pt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GendraPtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GendraPtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jerv/gendra_pt/GendraPtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jerv.gendra_pt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GendraPtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
